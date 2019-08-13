072b293bff80bd9b00cc6f8489e733535ab908b6
Vec <67650526@qq.com>
2019-08-13 23:59:05 +0800

    one

---
+++ src/main/java/com/vec/break_wifi/BreakWifiApplication.java
@@ -0,0 +1,13 @@
+package com.vec.break_wifi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakWifiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakWifiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vec/break_wifi/BreakWifiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.vec.break_wifi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BreakWifiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	    System.err.println("demo");
+	}
+
+}
